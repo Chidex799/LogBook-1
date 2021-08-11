920ca7a3b189fc33e828ae51bcfc7f9cd3ab180c
Chidi <praisechidi2002@gmail.com>
2021-08-11 14:12:40 +0100

    serializer commit

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from .models import User
+from .models import InstitutionSupervisor
+from .models import UniversitySupervisor
+
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
@@ -9,4 +12,17 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'uuid', 'first_name', 'last_name', 'phone_number', 'password']
-  
+
+
+class InstitutionSupervisorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = InstitutionSupervisor
+        fields = ['id', 'user', 'institution']
+
+
+class UniversitySupervisorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = UniversitySupervisor
+        fields = ['id', 'user', 'university', 'department']
