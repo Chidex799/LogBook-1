cbec36692516229d865c2532081b52afe39022b2
Chidi <praisechidi2002@gmail.com>
2021-08-16 09:07:39 +0100

    uni and inst serializers resolved

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,15 +1,8 @@
 from django.db import models
 from django.db.models import fields
 from rest_framework import serializers
-<<<<<<< HEAD
-from .models import User
-from .models import InstitutionSupervisor
-from .models import UniversitySupervisor
 
-=======
->>>>>>> f421f987bfa2be4f0d59a2b1f65eaf9bc0a75f99
-
-from .models import InstitutionSupervisor, User, Students
+from .models import InstitutionSupervisor, User, Students, UniversitySupervisor
 
 
 
@@ -17,7 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'uuid', 'first_name', 'last_name', 'phone_number', 'password']
-<<<<<<< HEAD
+
 
 
 class InstitutionSupervisorSerializer(serializers.ModelSerializer):
@@ -32,7 +25,7 @@ class UniversitySupervisorSerializer(serializers.ModelSerializer):
     class Meta:
         model = UniversitySupervisor
         fields = ['id', 'user', 'university', 'department']
-=======
+
   
 
 
@@ -41,4 +34,4 @@ class StudentsSerializer(serializers.ModelSerializer):
         model = Students
         fields = '__all__'
         read_only_fields = ("InstitutionSupervisor", "universityInspec")
->>>>>>> f421f987bfa2be4f0d59a2b1f65eaf9bc0a75f99
+
