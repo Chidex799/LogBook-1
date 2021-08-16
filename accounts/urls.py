9e4c46ff3a7519fb8e1619762a7f395442af7920
STEVEN AWOMODDU <STEVEN AWOMODDU@LAPTOP-KQBPAU8P>
2021-08-16 12:53:03 +0100

    Institution supervisor views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import StudentView
+from .views import StudentView, InstitutionSupervisorView
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register("students",StudentView, basename="students")
+router.register("InstitutionSupervisor",InstitutionSupervisorView, basename="InstitutionSupervisor")
 
 urlpatterns =  router.urls
