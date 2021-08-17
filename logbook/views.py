f55c213cd1316d7b77728d2b9988f3f0e18154cd
Igbide Orooghene <igbideoro24@gmail.com>
2021-08-17 12:42:28 +0100

    Specific Entry View

--- logbook/views.py
+++ logbook/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .serializers import EntrySerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import serializers, status
 
 # Create your views here.
+class SpecificEntry(APIView):
+    def put(self, request, format=None):
+        serializer = EntrySerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
+
