8e498edde9259debd2c44d2a0e19bab159910f91
Oforkansi Shadrach <oforkansi.shadrach@gmail.com>
2021-08-17 12:41:00 +0100

    entry model modified

--- logbook/models.py
+++ logbook/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from django.db.models.deletion import CASCADE
 from accounts.models import Students
 # Create your models here.
 class Entry(models.Model):
-    weekNumber=models.IntegerField()
+    weekNumber=models.IntegerField(blank = False, null = False)
     dateTime = models.DateTimeField(auto_now_add=True)
-    description=models.TextField()
-    image=models.ImageField()
-    studentID=models.ForeignKey(Students, on_delete=models.CASCADE)
-    remarks=models.TextField()
+    description=models.TextField(blank = False, null = False)
+    image=models.URLField(blank = True, null= True)
+    student=models.ForeignKey(Students, on_delete=models.CASCADE)
+    remarks=models.TextField(blank = True, null = True)
--- src/settings.py
+++ src/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'cloudinary',
     'accounts.apps.AccountsConfig',
+    'logbook.apps.LogbookConfig',
 ]
 
 MIDDLEWARE = [
