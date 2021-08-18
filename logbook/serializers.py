188f968e89776dd02bd65cc02f1b558597d951b1
STEVEN AWOMODDU <STEVEN AWOMODDU@LAPTOP-KQBPAU8P>
2021-08-18 23:33:00 +0100

    student and institution seperate entries

--- logbook/serializers.py
+++ logbook/serializers.py
@@ -3,7 +3,15 @@ from django.db.models import fields
 from rest_framework import serializers
 from .models import Entry
 
-class EntrySerializer(serializers.ModelSerializer):
+class StudentsEntrySerializer(serializers.ModelSerializer):
     class Meta:
         model = Entry
         fields = ['id','weekNumber', 'dateTime', 'description', 'image', 'student', 'remarks']
+        read_only_fields = ['remarks']
+
+
+class InstitutionSupervisorEntrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Entry
+        fields = ['id','weekNumber', 'dateTime', 'description', 'student', 'remarks']
+
